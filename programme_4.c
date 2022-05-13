c2742446dfc16493eaec63c89385967d6855b0bb
Esraq <esraq26552@gmail.com>
2022-05-13 15:37:56 +0600

    Input output of two integer number

---
+++ programme_4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b;
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	printf("Value of a=%d\n",a);
+	printf("Value of b=%d",b);
+	
+	return 0;
+	
+}
