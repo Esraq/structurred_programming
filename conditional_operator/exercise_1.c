a9b061c398018ecda412e5b7ecbf0c837c91041d
esraq <esraq26552@gmail.com>
2022-05-21 09:35:51 +0600

    commit from class

---
+++ conditional_operator/exercise_1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b;
+	
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	
+	if(a>b){
+		
+		
+		printf("a is bigger");
+	}
+	else{
+		
+		printf("b is bigger");
+		
+	}
+	
+	
+	return 0;
+	
+	
+}
