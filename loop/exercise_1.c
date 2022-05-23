ee9f9c128d9d1090b6f76aa5e2f503ac8372777f
esraq <esraq26552@gmail.com>
2022-05-23 11:08:45 +0600

    loop with break

---
+++ loop/exercise_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	
+	for(i=0;i<100;i++){
+		
+		if(i==5){
+			
+			break;
+		}
+		
+		printf("Angel\n");
+		
+	}
+	
+	printf("End of for loop");
+	
+	return 0;
+	
+	
+}
