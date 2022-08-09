e0fef3c45800f60b9ec375fe8c00bec22e1e5b65
Esraq Humayun Eisty <33447246+Esraq@users.noreply.github.com>
2022-08-09 11:55:37 +0600

    Update exercise_6.c

--- class_1/exercise_6.c
+++ class_1/exercise_6.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 int main()
 {
-	int c,d;
+	int c,d,temp;
 	
 	
 	printf("Enter the value of c:");
 	scanf("%d",&c);
 	printf("Enter the value of d:");
 	scanf("%d",&d);
-	printf("c=%d\nd=%d",d,c);
+	
+	temp=c;
+	c=d;
+	d=temp;
+	
+	printf("c=%d\n",c);
+	printf("d=%d\n",d);
+	
+	
+	
 	
 	return 0;
 	
